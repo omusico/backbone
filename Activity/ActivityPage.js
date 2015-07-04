2018dcdbaad7a2796e72f32abbc4eac78f4bf06b
Khoa Phan <khoaphan89@gmail.com>
2015-07-04 14:12:01 -0700

    Merge view activity page into one

--- Activity/ActivityPage.js
+++ Activity/ActivityPage.js
@@ -2,8 +2,8 @@
 
 var React = require('react-native');
 var ScrollableTabView = require('react-native-scrollable-tab-view');
-var ActivityChart = require('./ActivityChart');
-var InActivityChart = require('./InActivityChart');
+var RNChart = require('react-native-chart');
+var Firebase = require('firebase');
 
 var {
   StyleSheet,
@@ -13,21 +13,58 @@ var {
   TouchableHighlight,
 } = React;
 
+var deviceWidth = (require('Dimensions').get('window').width * .95);
+var deviceHeight = (require('Dimensions').get('window').width * .70);
+
 var styles = StyleSheet.create({
   container: {
-    marginTop: -15,
+    flex: 1,
+  },
+  chart: {
+    margin: 10,
+    width: deviceWidth,
+    height: deviceHeight,
   },
 });
 
 var ActivityPage = React.createClass({
+  getInitialState: function() {
+    return {
+      chartData: null,
+      xLabels: null,
+    };
+  },
+  componentWillMount: function() {
+    var context = this;
+    var ref = new Firebase("https://sweltering-fire-6261.firebaseio.com/users/");
+    ref.child(this.props.userID).on('value', function(snapshot) {
+      var snapshotVal = snapshot.val();
+      console.log('USERID', + ' + ', context.props.userID, ' + ', 'SNAPSHOT!!! ', snapshotVal, ' + ', snapshotVal.activityData, ' + ', snapshotVal.activityDate);
+      context.setState({
+        chartData: [
+          {
+            name:'BarChart',
+            type:'bar',
+            color1:'#48BBEC',
+            color2: '#FFA500',
+            widthPercent:0.6,
+            data: snapshotVal.activityData,
+          },
+        ],
+        xLabels: snapshotVal.activityDate,
+      });
+    }, function(errorObject) {
+      console.log('The read failed: ', errorObject.code);
+    });
+  },
   render: function() {
     return (
-      <View style={styles.container}>
-        <ScrollableTabView>
-          <ActivityChart userID={this.props.userID} tabLabel="Activity" />
-          <InActivityChart userID={this.props.userID} tabLabel="InActivity" />
-        </ScrollableTabView>
-      </View>
+        <View style={styles.container}>
+          <RNChart style={styles.chart}
+            chartData={this.state.chartData}
+            xLabels={this.state.xLabels}>
+          </RNChart>
+        </View>
     )
   },
 })
